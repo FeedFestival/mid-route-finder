c18c816154278969e2b5c84e67aaa7328c78b043
FeedFestival <simionescudani07@gmail.com>
2026-02-01 13:50:25 +0200

    feat: added prettier for ui
    added prettier for ui
    
    Resolves fe

--- mid-route-finder-ui/src/app/app.ts
+++ mid-route-finder-ui/src/app/app.ts
@@ -3,15 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { Unity } from './Unity/unity';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, Unity],
-  templateUrl: './app.html',
-  styleUrl: './app.scss',
+	selector: 'app-root',
+	imports: [RouterOutlet, Unity],
+	templateUrl: './app.html',
+	styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('mid-route-finder-ui');
+	protected readonly title = signal('mid-route-finder-ui');
 
-  clicked(): void {
-    console.log('clicked: ', new Date().toISOString());
-  }
+	clicked(): void {
+		console.log('clicked: ', new Date().toISOString());
+	}
 }
